7d0b64e6d3eb38dae00ec2ce8f5b7f4a17140c7b
Anatoly V <ntnosteam@gmail.com>
2021-03-28 23:42:26 +0300

    Add func to insert a single record

--- database.py
+++ database.py
@@ -5,11 +5,10 @@ from sqlite3.dbapi2 import connect
 
 
 def show_all():
+    
 
-    # Connect to db
+    # Connect to db and create cursor
     conn = sqlite3.connect('customer.db')
-
-    # Create cursor
     cursor = conn.cursor()
 
     # Query db
@@ -25,6 +24,21 @@ def show_all():
 
     # Commit command
     conn.commit()
+
+    # Close connection
+    conn.close()
+
+def add_one(first, last, email):
+    # Add a new record to the table
+
+    # Connect to db and create cursor
+    conn = sqlite3.connect('customer.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+    --sql
+    INSERT into customers VALUES (?,?,?) 
+    ;
+    """, (first, last, email))
 
     # Close connection
     conn.close()
--- db_app.py
+++ db_app.py
@@ -2,4 +2,8 @@
 
 import database
 
+# add records to database
+database.add_one("Laura","Smith","laura@smith.com")
+
+# show all records
 database.show_all()
